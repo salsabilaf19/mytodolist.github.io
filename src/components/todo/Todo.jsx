2826b122c8b8dff69f25803899f2120ec5f63aae
salsabilaf <salsabilafithriyah19@student.uns.ac.id>
2022-10-27 16:12:10 +0700

    first commit

---
+++ src/components/todo/Todo.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import "./style.css";
+
+function Todo({ todo, onDeleteHanlder, onEditHandler }) {
+    return (
+        <div className="todo-container">
+            <div>
+                <h2 className="todo-title">{todo.title}</h2>
+                <div>{todo.body}</div>
+            </div>
+            <div className="button-set">
+                <button
+                    className="todo-delete-button button"
+                    onClick={() => onDeleteHanlder(todo.id)}
+                > Delete
+                </button>
+                <button
+                    className="todo-complete-button button"
+                    onClick={() => onEditHandler(todo.id)}
+                >
+                    {todo.isDone ? "Done" : "Done"}
+                </button>
+            </div>
+        </div>
+    );
+}
+
+export default Todo;
